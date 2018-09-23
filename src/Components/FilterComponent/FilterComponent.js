b5018e616820c3d2f320ccc01aa8fe41f80138a9
Enny <14zb@queensu.ca>
2018-09-23 08:25:05 -0400

    Fixed the position of the txt in the filter component

--- src/Components/FilterComponent/FilterComponent.js
+++ src/Components/FilterComponent/FilterComponent.js
@@ -7,29 +7,37 @@ class FilterComponent extends Component {
       <div className="FilterComponent">
         <div className="Filter-sidebar">
           <div className="sidenav">
-            <p className="category">School:</p>
+            <p className="category">School</p>
             <div className="sidebar-border">
+            <div className="filter-div">
               <a href="#">York University</a>
               <a href="#">University of Toronto</a>
               <a href="#">Ryerson University</a>
               <a href="#">Seneca College</a>
+              </div>
             </div>
-          <p className="category">Location:</p>
+          <p className="category">Location</p>
             <div className="sidebar-border">
+            <div className="filter-div">
               <a href="#">Toronto (GTA)</a>
               <a href="#">City of Toronto</a>
             </div>
-            <p className="category">Contract Length:</p>
+            </div>
+            <p className="category">Contract Length</p>
             <div className="sidebar-border">
+            <div className="filter-div">
               <a href="#">4 month contract</a>
               <a href="#">8 month contract</a>
               <a href="#">12 month contract</a>
             </div>
-            <p className="category"># of rooms available:</p>
+            </div>
+            <p className="category"># of rooms available</p>
             <div className="sidebar-border">
+            <div className="filter-div">
               <a href="#">1-3</a>
               <a href="#">4-6</a>
               <a href="#">6-8</a>
+            </div>
             </div>
           </div>
         </div>
