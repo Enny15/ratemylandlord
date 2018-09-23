42f18a6ca6be740c2886a47d78477cef64bd6a25
Enny <14zb@queensu.ca>
2018-09-22 23:29:00 -0400

    Added the homepage buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import Header from './Components/HeaderComponent/HeaderComponent';
+import SearchBar from './Components/SearchBarComponent/SearchBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <div className="button-div text-center">
+            <button type="button" className="btn btn-item">RATE A ROOM</button>
+            <button type="button" className="btn btn-item">RENT A ROOM</button>
+            <button type="button" className="btn btn-item">POST/SELL A ROOM</button>
+        </div>
       </div>
     );
   }
