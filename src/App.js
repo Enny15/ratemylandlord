d56a3d1582b390026cea6f00c6ff38130c2e6aab
Enny <14zb@queensu.ca>
2018-09-23 00:01:07 -0400

    Added search bar to the homepage

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <div className="search-div">
+          <SearchBar />
+        </div>
         <div className="button-div text-center">
             <button type="button" className="btn btn-item">RATE A ROOM</button>
             <button type="button" className="btn btn-item">RENT A ROOM</button>
